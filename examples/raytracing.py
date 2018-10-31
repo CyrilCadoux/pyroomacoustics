6cfc1654c2049f920b926b105265d7780631394f
Cyril Cadoux <cyril.cadoux@epfl.ch>
2018-10-31 19:08:45 +0100

    Save

--- examples/raytracing.py
+++ examples/raytracing.py
@@ -700,7 +700,7 @@ def wall_absorption(previous_energy, wall):
     return previous_energy * math.sqrt(1 - wall.absorption)
 
 
-def stop_ray(actual_travel_time, time_thresh, actual_energy, energy_thresh=0.1):
+def stop_ray(actual_travel_time, time_thresh, actual_energy, energy_thresh=0.4):
     """
     Returns True if the ray must be stopped according to the 'which' condition
     :param actual_travel_time: total travel time of the ray from the source to the point of evaluation
@@ -817,7 +817,7 @@ def draw_segment(source, hit, red=True):
         raise ValueError("The function draw_segment only supports points of same dimension (2 or 3)")
 
 
-def highpass(audio, fs, cutoff=200, butter_order=5):
+def highpass(audio, fs, cutoff=100, butter_order=5):
     nyq = 0.5 * fs
     fc_norm = cutoff / nyq
     b, a = signal.butter(butter_order, fc_norm, btype="high", analog=False)
@@ -1086,13 +1086,14 @@ def get_rir_rt(room,
     return ir
 
 
-def apply_rir(rir, wav_data, fs=16000, result_name="result.wav"):
+def apply_rir(rir, wav_data, fs=16000, cutoff=100, result_name="aaa.wav"):
     """
     This function applies a RIR to sound data coming from a .wav file
     The result is written in the current directory
     :param rir: the room impulse response
     :param wav_data: an array of data with wav format
     :param fs: the sampling frequency used to write the result in local directory
+    :param cutoff: the cutoff frequency of the high pass filter
     :param result_name: the name of the resulting .wav file
     :return: Nothing
     """
@@ -1100,7 +1101,7 @@ def apply_rir(rir, wav_data, fs=16000, result_name="result.wav"):
     # Compute the convolution and set all coefficients between -1 and 1 (range for float32 .wav files)
     result = scipy.signal.fftconvolve(rir, wav_data)
 
-    result = highpass(result, fs)
+    result = highpass(result, fs, cutoff)
 
     result /= np.abs(result).max()
     result -= np.mean(result)
@@ -1115,14 +1116,14 @@ nb_phis = 1000
 nb_thetas = 25 if _3D else 1
 
 scatter_coef = 0.1
-absor = 0.1
+absor = 0.01
 init_energy = 1000
 ray_simul_time = 2.
 
 
 fs0, audio_anechoic = wavfile.read(os.path.join(os.path.dirname(__file__),"input_samples", 'moron_president.wav'))
 
-size_factor = 3.
+size_factor = 4.
 audio_anechoic = audio_anechoic[:,0]
 audio_anechoic = audio_anechoic-np.mean(audio_anechoic)
 pol = size_factor * np.array([[0., 0.], [0., 1.], [1., 1.], [1., 0.]]).T
@@ -1151,7 +1152,7 @@ else:
 
     # Add the circular microphone
     mic_pos = np.array([2.,2.])
-    mic_radius = 0.4  # meters
+    mic_radius = 0.05  # meters
     source = [1,1]
 
     # Create the room from its corners
@@ -1171,10 +1172,10 @@ if dist(mic_pos, source) <= mic_radius:
 
 rir_rt = get_rir_rt(room, nb_phis, ray_simul_time, init_energy, mic_pos, mic_radius, scatter_coef, nb_thetas=nb_thetas, plot_rays=False, plot_RIR=True)
 
-apply_rir(rir_rt, audio_anechoic, fs = fs0, result_name='aaa.wav')
+apply_rir(rir_rt, audio_anechoic, fs = fs0, cutoff=300, result_name='aaa.wav')
 # result_name=d+"_"+str(nb_thetas*nb_phis)+"rays""_absor" + str(absor) +"_scat"+ str(scatter_coef)+".wav"
 
 # Image source method
 # plt.figure()
 # room.plot_rir()
-# plt.show()
+# plt.show()c
